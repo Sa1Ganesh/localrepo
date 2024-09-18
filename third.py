4da95ed121fb299f464242861eaabd53c0586427
Sa1Ganesh <saiganesh1w@gmail.com>
2024-09-19 01:58:21 +0530

    modified

--- third.py
+++ third.py
@@ -1,1 +1,36 @@
 print("sai ram")
+'''
+#lists
+marks=[95,94.6,85,86.7]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(len(marks))
+student=["sai",85,"delhi",1.0]
+print(student)
+student[0]="sathya" #mutable
+print(student)
+#list slicing
+print(marks[0:2])
+print(marks[:3])
+print(marks[0:])
+print(marks[-3:-1])
+#list methods
+list=[2,1,3,6]
+#list.append()
+list.append(7)
+print(list)
+#list.sort()
+list.sort()
+print(list)
+#list.sort(reverse=True)
+list.sort(reverse=True)
+print(list)
+#list.reverse()
+list.reverse()
+print(list)
+#list.insert(idx,el)
+list.insert(5,10)
+print(list)
+'''
