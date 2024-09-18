58ca33ee7028330f40a1cfd1e559f267e51913b1
Sa1Ganesh <saiganesh1w@gmail.com>
2024-09-18 19:32:53 +0530

    modified

--- second.py
+++ second.py
@@ -1,1 +1,48 @@
 print("sai ram")
+#str1="this is a string"
+#str2='student'
+#str3='''string'''
+'''str1="love all serve all .\thelp ever hurt never"
+print(str1)
+str2="love all serve all .\nhelp ever hurt never"
+print(str2)'''
+#concatenation
+'''str1="sai"
+len1=len(str1)
+str2="ram"
+len2=len(str2)
+final_str=str1+" "+str2
+len3=len(final_str)
+print(final_str)
+print(len1)
+print(len2)
+print(len3)'''
+#index
+'''str="sathya"
+ch=str[1]
+print(ch)'''
+#sciling
+'''str="sairam ganesh"
+print(str[0:3])
+print(str[:6])
+print(str[7:])
+print(str[-5:-1])'''
+#string functions
+'''str="i am a cricketer.i am self confident"
+#1 str.endswith("")
+print(str.endswith("nt"))
+#2 str.capitalize()
+print(str.capitalize())
+#3 str.replace(old,new)
+print(str.replace("a","@"))
+#4 str.find(word)
+print(str.find("self"))
+#5 str.count("")
+print(str.count("am"))'''
+#p1
+name=input("enter your first name: ")
+print("f_name:",name)
+print("length :" ,len(name))
+print("occurence:",name.count("a"))
+print("position:",name.find("t"))
+
