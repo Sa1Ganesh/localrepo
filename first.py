952843f84ed70f38ec6997e8136b710e7335edfb
Sa1Ganesh <saiganesh1w@gmail.com>
2024-09-18 15:31:02 +0530

    modified

--- first.py
+++ first.py
@@ -1,9 +1,13 @@
 name="sai"
-age=23
+age=22
 price=25.32
+a=None
+b=True
 print("may name is :",name)
 print("my age is :",age)
 print(price)
 print(type(name))
 print(type(age))
 print(type(price))
+print(type(a))
+print(type(b))
