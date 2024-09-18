8b9d7ac9e630f3ecefd4b28103bc5aae006ccb6b
Sa1Ganesh <saiganesh1w@gmail.com>
2024-09-19 04:35:52 +0530

    4

--- fourth.py
+++ fourth.py
@@ -1,2 +1,45 @@
 print("sairam")
-#dictinary and set
+#dictinary (key:value pairs)
+#unorderd,mutable,no duplicate keys
+'''
+info={
+    "key":"value",
+    "name":"sathya",
+    "place":"puttaparthi",
+    "age":22,
+    "is_adult":True,
+    "marks":[25,24.5],
+    "grade":("A","B"),
+    1:"sai"
+}
+print(info)
+print(type(info))
+print(info["name"])
+info["name"]:"sai"
+print(info)
+null_dict={}
+print(null_dict)
+'''
+#nested dictionaries
+student={
+    "name":"ganesh",
+    "subjects":{
+        "maths":74,
+        "phy":59,
+        "che":60
+    }
+}
+print(student)
+print(student["subjects"]["che"])
+#dictionary methods
+#mydict.keys()
+print(student.keys())
+#mydict.values()
+print(student.values())
+#mydict.items()
+print(student.items())
+#mydict.get("key")
+print(student.get("name"))
+#mydict.update(newdict)
+student.update("place":"puttaparthi")
+print(student)
