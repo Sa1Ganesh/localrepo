30e5439a3cec1a3036caa8c77d4eaf4fdea6b2e4
Sa1Ganesh <saiganesh1w@gmail.com>
2024-09-19 23:41:10 +0530

    7

--- seventh.py
+++ seventh.py
@@ -1,1 +1,22 @@
 print("sairam")
+#file I/O
+#first open 
+#read(r)
+'''
+f=open("sam.txt","r") #rt=r (t=text mode)
+#data=f.read()
+#d1=f.read(8)
+line1=f.readline()
+#print(data)
+#print(d1)
+print(line1)
+#print(type(data))
+f.close()
+'''
+#write(w) 
+f=open("sam.txt","a")
+#wrt=f.write("myself sai ganesh ,i am learning python")
+app=f.write("\nthank you")
+f.close()
+
+
