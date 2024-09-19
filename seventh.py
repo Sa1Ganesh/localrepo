bd66dcb4172de8f5da417b1514ab6ba2b702f761
Sa1Ganesh <saiganesh1w@gmail.com>
2024-09-20 00:06:23 +0530

    7

--- seventh.py
+++ seventh.py
@@ -13,10 +13,41 @@ print(line1)
 #print(type(data))
 f.close()
 '''
-#write(w) 
+'''#write(w)  
+#f=open("sam.txt","w")
+#append(a)
 f=open("sam.txt","a")
 #wrt=f.write("myself sai ganesh ,i am learning python")
 app=f.write("\nthank you")
 f.close()
+'''
+'''
+f=open("sam.txt","r+") #read and write
+f.write("love")
+print(f.read())
+f.close()
+'''
+'''
+f=open("sam.txt","w+") #write and read
+f.write("sai")
+print(f.read())
+f.write("sathya")
+f.close()
+'''
+'''
+f=open("sam.txt","a+") #append ,write and read
+#f.write("sai")
+print(f.read())
+f.write("narayana")
+f.close()
+'''
+#r+ read+ overwrite (ptr start) -no truncate
+#w+ read+ overwrite (ptr start) -truncate
+#a+ read+ append (ptr end) -no truncate
 
-
+#with synatx
+with open("sam.txt","r") as f:
+    data=f.read()
+    print(data)
+with open("sam.txt","w") as f:
+    f.write("sathya narayana ")
