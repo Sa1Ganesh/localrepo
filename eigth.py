0e29ba664a2c7e9c31d18f1872921cd12540b5af
Sa1Ganesh <saiganesh1w@gmail.com>
2024-09-20 13:01:49 +0530

    8

--- eigth.py
+++ eigth.py
@@ -32,6 +32,7 @@ c1=Car()
 print(c1.color)
 print(c1.brand)
 '''
+'''
 #p1
 class Student:
     def __init__(self,name,marks):
@@ -42,7 +43,53 @@ class Student:
         for val in self.marks:
             sum+=val
         print("hi",self.name,"your avg score is",sum/3)
-
+    @staticmethod
+    def college(): #static methods
+        print("jain")
 
 s1=Student("sai",[95,96,98])
 s1.get_avg()
+s1.college()
+'''
+#abstraction and
+#encapsulation
+class Car:
+    def  __init__(self):
+        self.acc=False
+        self.brk=False
+        self.clutch=False
+    def start(self):
+        self.clutch=True
+        self.acc=True
+        print("car started ..") 
+car1=Car()
+car1.start()           
+#p1
+class Account:
+    def __init__(self,bal,acc):
+        self.balance=bal
+        self.account_no=acc
+
+    #debit method
+    def debit(self,amount):
+        self.balance-=amount
+        print("RS.",amount ,"was debited")
+        print("total balance",self.get_balance())
+    #credit method
+    def credit(self,amount):
+        self.balance+=amount
+        print("RS.",amount ,"is credited")
+        print("total balance",self.get_balance()) 
+    def get_balance(self):
+        return self.balance
+
+acc1=Account(10000,12345)
+print(acc1.balance)
+print(acc1.account_no)
+acc1.debit(1000)
+acc1.credit(5000)
+
+
+
+
+
